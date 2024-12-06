73d5489dfea50d59a9e9a4de0b4b6b297909490b
Sumon Ahmed <csesumon8@gmail.com>
2024-12-06 21:46:13 +0600

    individual loop with function

---
+++ loop.dart
@@ -0,0 +1,23 @@
+void main() {
+  for_loop();
+  for_in_loop();
+  while_loop();
+  do_while_loop();
+}
+
+void for_loop() {
+  var i = 1;
+  print("this is for loop " + i.toString());
+}
+
+void for_in_loop() {
+  print("This is for in loop");
+}
+
+void while_loop() {
+  print("This is for while lopp");
+}
+
+void do_while_loop() {
+  print("This is for do while loop");
+}
